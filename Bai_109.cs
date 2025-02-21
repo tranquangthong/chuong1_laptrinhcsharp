c83dd43e7a8cc938c24e279845d8e40d7c6e0987
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 22:17:02 +0700

    Add files via upload
    Thuật toán in bảng cửu chương cố định

---
+++ Bai_109.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int so_141 = 2; 
+        Console.WriteLine($"Bang cuu chuong {so_141}:");
+
+        for (int i_141 = 1; i_141 <= 10; i_141++)
+        {
+            Console.WriteLine($"{so_141} x {i_141} = {so_141 * i_141}");
+        }
+    }
+}
