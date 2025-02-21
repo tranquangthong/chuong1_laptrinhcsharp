6cb3ffeef95020a0e32471f70d7be5cda58a0eac
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 20:47:46 +0700

    Add files via upload
    Thêm thuật toán tính tổng ước số bằng vòng lặp for

---
+++ Bai21_c1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int tong_141 = 0; 
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0) 
+            {
+                tong_141 += i;
+            }
+        }
+
+        Console.WriteLine($"Tong cac uoc cua {n} = {tong_141}");
+    }
+}
