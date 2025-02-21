adb2859dd0f1c88d4f315d3af1badccb253ee8fd
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 20:25:50 +0700

    Add files via upload
    Tỉnh tổng bằng vòng lặp for (tính giai thừa rồi thực hiện tính tổng)

---
+++ Bai11.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        long giaiThua = 1;
+        long tong_141 = 0; 
+
+        for (int i = 1; i <= n; i++)
+        {
+            giaiThua *= i;
+            tong_141 += giaiThua;
+        }
+
+        Console.WriteLine($"Tong S(n) = {tong_141}");
+    }
+}
