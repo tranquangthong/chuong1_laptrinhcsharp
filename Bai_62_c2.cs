e7c1fa6ad02b0e04d6def70818202453ab255ac6
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:47:42 +0700

    Add files via upload
    Cách tìm ucln bằng thuật toán Euclid
    Nếu b = 0, trả về a, ngược lại, gọi đệ quy UCLN(b, a % b)

---
+++ Bai_62_c2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static int UCLN(int a, int b)
+    {
+        return b == 0 ? a : UCLN(b, a % b);
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int ucln_141 = UCLN(a, b); 
+
+        Console.WriteLine($"Uoc chung lon nhat cua {a} va {b} la: {ucln_141}");
+    }
+}
