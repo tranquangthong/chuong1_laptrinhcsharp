170169b548db6b2414b8929d02b173962b26994a
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 20:50:19 +0700

    Add files via upload
    Tối ưu thuật toán: Giảm số lần lặp bằng cách duyệt đến √n

---
+++ Bai21_c2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int tong_141 = 0;
+
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                tong_141 += i;
+                if (i != n / i) 
+                {
+                    tong_141 += n / i;
+                }
+            }
+        }
+
+        Console.WriteLine($"Tong cac uoc cua {n} = {tong_141}");
+    }
+}
