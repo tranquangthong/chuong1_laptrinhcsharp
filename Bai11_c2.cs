694c4e6adaa88cddc711fa072e0e4e8d42309127
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 20:35:39 +0700

    Add files via upload
    Cách 2, xử dụng đệ quy để tính giai thừa, dùng vòng lặp for để tính tổng

---
+++ Bai11_c2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static long GiaiThua(int x)
+    {
+        if (x == 1) return 1;
+        return x * GiaiThua(x - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        long tong_141 = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            tong_141 += GiaiThua(i);
+        }
+
+        Console.WriteLine($"Tong S(n) = {tong_141}");
+    }
+}
