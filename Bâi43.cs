257a3537d02f2be21c36f5d06e4477dd769c849c
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:13:33 +0700

    Add files via upload
    Thuật toán đếm chữ số bằng vòng lặp while

---
+++ Bâi43.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int so_luong_chu_so_141 = 0;
+
+        int temp = n;
+        while (temp > 0)
+        {
+            so_luong_chu_so_141++;
+            temp /= 10;
+        }
+
+        Console.WriteLine($"So luong chu so cua {n} la: {so_luong_chu_so_141}");
+    }
+}
