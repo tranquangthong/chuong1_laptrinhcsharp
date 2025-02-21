770d5d3e060cf18bf0b36fc9d725b53d49468dc7
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 22:33:08 +0700

    Add files via upload
    Bắt đầu từ 1, liên tục nhân 2 đến khi đạt hoặc vượt qua số cần kiểm tra

---
+++ Bai_75.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static bool LaSoDangHaiMuK(int n_141)
+    {
+        if (n_141 <= 0) return false; 
+
+        int so_141 = 1;
+        while (so_141 < n_141)
+        {
+            so_141 *= 2;
+        }
+        return so_141 == n_141;
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap so nguyen duong: ");
+        int n_141 = int.Parse(Console.ReadLine());
+
+        if (LaSoDangHaiMuK(n_141))
+            Console.WriteLine($"{n_141} la so dang 2^k.");
+        else
+            Console.WriteLine($"{n_141} khong phai so dang 2^k.");
+    }
+}
