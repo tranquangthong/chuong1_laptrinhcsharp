783ae845c8fb06c3b3ef1c085127a245767be968
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 20:53:46 +0700

    Update Bai21_c2.cs
    Tối ưu thuật toán: Giảm số lần lặp bằng cách duyệt đến √n
    Thêm xử lý ngoại lệ khi nhập dữ liệu

--- Bai21_c2.cs
+++ Bai21_c2.cs
@@ -1,26 +1,39 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Nhap n: ");
-        int n = int.Parse(Console.ReadLine());
-
-        int tong_141 = 0;
-
-        for (int i = 1; i * i <= n; i++)
+        try
         {
-            if (n % i == 0)
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 1)
             {
-                tong_141 += i;
-                if (i != n / i) 
+                Console.WriteLine("Vui long nhap so nguyen duong!");
+                return;
+            }
+
+            int tong_141 = 0;
+
+            for (int i = 1; i * i <= n; i++)
+            {
+                if (n % i == 0)
                 {
-                    tong_141 += n / i;
+                    tong_141 += i;
+                    if (i != n / i)
+                    {
+                        tong_141 += n / i;
+                    }
                 }
             }
-        }
 
-        Console.WriteLine($"Tong cac uoc cua {n} = {tong_141}");
+            Console.WriteLine($"Tong cac uoc cua n {n} = {tong_141}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Loi nhap lieu!");
+        }
     }
 }
