7f3e3b1b941cba6b2bf49aa5d4b4a8c666c940ed
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:46:01 +0700

    Add files via upload
    Thuật toán tìm ucln bằng phương pháp trừ dần
    So sánh a và b, trừ số lớn cho số nhỏ cho đến khi hai số bằng nhau

---
+++ Bai_62.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int ucln_141;
+        int x = a, y = b; // Biến có 3 số cuối mã sinh viên
+
+        while (x != y)
+        {
+            if (x > y) x -= y;
+            else y -= x;
+        }
+        ucln_141 = x;
+
+        Console.WriteLine($"Uoc chung lon nhat cua {a} va {b} la: {ucln_141}");
+    }
+}
