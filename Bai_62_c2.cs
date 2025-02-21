cda2af92e5f092f6a7505d2340de8813fd16ef6d
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:52:18 +0700

    Update Bai_62_c2.cs
    Thêm xử lý ngoại lệ
    Thêm chức năng nhập nhiều lần

--- Bai_62_c2.cs
+++ Bai_62_c2.cs
@@ -9,14 +9,31 @@ class Program
 
     static void Main()
     {
-        Console.Write("Nhap a: ");
-        int a = int.Parse(Console.ReadLine());
+        while(true)
+        {
+            try
+            {
+                Console.Write("Nhap a (nhap 0 0 de thoat): ");
+                int a = int.Parse(Console.ReadLine());
 
-        Console.Write("Nhap b: ");
-        int b = int.Parse(Console.ReadLine());
+                Console.Write("Nhap b: ");
+                int b = int.Parse(Console.ReadLine());
 
-        int ucln_141 = UCLN(a, b); 
+                if (a == 0 && b == 0) break;
+                if (a < 1 || b < 1)
+                {
+                    Console.WriteLine("Vui long nhap hai so nguyen duong!");
+                    continue;
+                }
 
-        Console.WriteLine($"Uoc chung lon nhat cua {a} va {b} la: {ucln_141}");
+                int ucln_141 = TimUCLN(a, b);
+
+                Console.WriteLine($"Uoc chung lon nhat cua {a} va {b} la: {ucln_141}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Loi nhap lieu! Vui long nhap so nguyen hop le.");
+            }
+        }
     }
 }
