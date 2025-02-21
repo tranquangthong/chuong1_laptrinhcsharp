bd042cbee9b028e21212e29f6fea78801c9924da
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 22:57:00 +0700

    Update Bai43_c2.cs
    Update code Bai43_c2

--- Bai43_c2.cs
+++ Bai43_c2.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Bai43
+class Program
 {
-    class Bai43_c2
+    static int DemSoChuSo(int n)
     {
+        return n.ToString().Length;
+    }
+
+    static void Main()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Nhap n (nhap 0 de thoat): ");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n == 0) break;
+                if (n < 1)
+                {
+                    Console.WriteLine("Vui long nhap so nguyen duong!");
+                    continue;
+                }
+
+                int so_luong_chu_so_141 = DemSoChuSo(n);
+
+                Console.WriteLine($"So luong chu so cua {n} la: {so_luong_chu_so_141}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Loi nhap lieu! Vui long nhap so nguyen hop le.");
+            }
+        }
     }
 }
