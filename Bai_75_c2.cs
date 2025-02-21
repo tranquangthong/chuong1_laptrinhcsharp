6fb25470e28d26e1f7f973852ddefc308ecc7dd5
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 22:36:11 +0700

    Add files via upload
    Tối ưu thuật toán bằng toàn tử bit

---
+++ Bai_75_c2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static bool LaSoDangHaiMuK(int n_141)
+    {
+        return n_141 > 0 && (n_141 & (n_141 - 1)) == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap so nguyen duong: ");
+        int n_141 = int.Parse(Console.ReadLine());
+
+        if (LaSoDangHaiMuK(n_141))
+            Console.WriteLine($"{n_141} la so dang 2^k.");
+        else
+            Console.WriteLine($"{n_141} khong phai so dang 2^k.");
+    }
+}
