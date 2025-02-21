ff4da1f6614012e3a01614ed8b57c6b439d32c94
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 22:03:01 +0700

    Add files via upload
    them vong lap for xuat ky tu tu A den Z

---
+++ Bai_88.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Cac ky tu tu A den Z:");
+
+        for (char ky_tu_141 = 'A'; ky_tu_141 <= 'Z'; ky_tu_141++)
+        {
+            Console.Write(ky_tu_141 + " ");
+        }
+    }
+}
