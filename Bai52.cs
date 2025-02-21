3742aa4bb94fde317f35700e46bb099881901d33
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:27:22 +0700

    Add files via upload
    Thuật toán tìm chữ số nhỏ nhất bằng vòng lặp for
    Lấy từng chữ số của n, so sánh và tìm giá trị nhỏ nhất

---
+++ Bai52.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int chu_so_nho_nhat_141 = 9; 
+
+        int temp = n;
+        while (temp > 0)
+        {
+            int chu_so = temp % 10;
+            if (chu_so < chu_so_nho_nhat_141)
+            {
+                chu_so_nho_nhat_141 = chu_so;
+            }
+            temp /= 10;
+        }
+
+        Console.WriteLine($"Chu so nho nhat cua {n} la: {chu_so_nho_nhat_141}");
+    }
+}
