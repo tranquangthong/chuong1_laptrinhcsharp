07ec2d43732de14aa1a1673202eb73c4f21777d4
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:39:00 +0700

    Update Bai52_c2.cs
    tach thuat toan tim chu so nho nhat thanh ham rieng
    them truong hop xu ly ngoai le

--- Bai52_c2.cs
+++ Bai52_c2.cs
@@ -3,13 +3,31 @@ using System.Linq;
 
 class Program
 {
+    static int TimChuSoNhoNhat(int n)
+    {
+        return n.ToString().Min(c => c - '0');
+    }
     static void Main()
     {
-        Console.Write("Nhap n: ");
-        int n = int.Parse(Console.ReadLine());
+        while(true)
+        {
+            try
+            {
+                Console.Write("Nhap n(Nhap 0 de thoat):");
+                int n = int.Parse(Console.ReadLine());
+                if(n==0) break;
+                if(n<1)
+                {
+                    Console.WriteLine("Vui long nhap so nguyen duong!");
+                    continue;
+                }
+                int chu_so_nho_nhat_141=TimChuSoNhoNhat(n);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Loi nhap lieu! Vui long nhap so nguyen hop le.");
+            }
+        }
 
-        int chu_so_nho_nhat_141 = n.ToString().Min(c => c - '0'); 
-
-        Console.WriteLine($"Chu so nho nhat cua {n} la: {chu_so_nho_nhat_141}");
     }
 }
