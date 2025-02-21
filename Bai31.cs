32ae9ff8a3c0fe8690d8a79b9de46546a0701c1d
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-21 21:01:57 +0700

    Add files via upload
    Thuật toán kiểm tra số nguyên tố bằng vòng lặp for

---
+++ Bai31.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        bool la_so_nguyen_to_141 = true; 
+
+        if (n < 2)
+        {
+            la_so_nguyen_to_141 = false;
+        }
+        else
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    la_so_nguyen_to_141 = false;
+                    break;
+                }
+            }
+        }
+
+        if (la_so_nguyen_to_141)
+            Console.WriteLine($"{n} la so nguyen to");
+        else
+            Console.WriteLine($"{n} khong phai so nguyen to");
+    }
+}
